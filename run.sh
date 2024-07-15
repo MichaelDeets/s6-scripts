13a33a02679a0e5810591f6340710d6558c368fd
MichaelDeets <michael@deets.sh>
2024-07-15 21:28:49 +0100

    Undo test

--- run.sh
+++ run.sh
@@ -5,4 +5,3 @@ mkdir -p /etc/s6-rc/
 ln -sf /etc/s6-linux-init/compiled/ /etc/s6-rc/
 s6-linux-init-maker -G "/sbin/agetty tty12 38400 linux" -1 "/etc/s6-linux-init/current"
 cp -rf /etc/s6-linux-init/current/bin/* /usr/local/sbin/
-#test
